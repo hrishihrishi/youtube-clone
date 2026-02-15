fcd8a81092e115b22534d7959d5e0199bb8adc56
Hrishikesh <hrishi.halesh.18@gmail.com>
2026-02-15 11:59:33 +0530

    setup tailwind and redux

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>frontend</h1>
+      <button className="btn">Click me</button>
+    </div>
+  )
+}
+
+export default App
